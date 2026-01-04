821744f39bd115b076c809563049d1cd6f1e09d4
Alex Kusov <kysov2006@bk.ru>
2026-01-04 18:22:31 +0300

    auth form

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,33 @@ import './assets/main.css'
 import Aura from '@primeuix/themes/aura'
 import 'primeicons/primeicons.css'
 
+import { initializeApp } from "firebase/app";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+
+
 import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
+import Inputtext from 'primevue/inputtext'
+
 
 import App from './App.vue'
 import router from './router'
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjtMjtWb6URw7aFqDrbY5XeNVK0U2LpaM",
+  authDomain: "hh-proj-ba0a8.firebaseapp.com",
+  projectId: "hh-proj-ba0a8",
+  storageBucket: "hh-proj-ba0a8.firebasestorage.app",
+  messagingSenderId: "352657830052",
+  appId: "1:352657830052:web:302dab2618eb1b0a433631"
+};
+
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
 
@@ -22,5 +41,7 @@ app.use(PrimeVue, {
 })
 
 app.component('app-menubar', Menubar)
+app.component('app-button', Button)
+app.component('app-inputtext', Inputtext)
 
 app.mount('#app')
--- src/views/PageAuth.vue
+++ src/views/PageAuth.vue
@@ -1,6 +1,51 @@
 <script setup lang="ts">
+  import {computed, ref} from 'vue'
+
+  const isLogin = ref<boolean>(true)
+  const email = ref<string>('')
+  const password = ref<string>('')
+  const isLoading = ref<boolean>(false)
+
+  const toggleAuth = () => {
+    isLogin.value = !isLogin.value;
+  }  
+
+  const subtitleText = computed<string>(() =>{
+    return isLogin.value ? 'Аккаунта еще нет' : 'Уже есть аккаунт?'
+  })
+
+  const linkAccountText = computed<string>(() => {
+    return isLogin.value ? 'Создайте сейчас' : 'Войдите в аккаунт'
+  })
+
+  const submitButtonText = computed<string>(() => {
+    return isLogin.value ? 'Создать' : 'Войти'
+  })
+
+  const submitForm = (): void => {
+    console.log('submit')
+  }
+
 </script>
 
 <template>
-  Auth
+  <div class="auth_wrap">
+    <div>
+        <h1>Добро пожаловать</h1>
+        <span>{{ subtitleText }}</span> <span @click="toggleAuth">{{ linkAccountText }}</span>
+        <form @submit.prevent="submitForm">
+          <div>
+            <label for="email-field">Email</label>
+            <app-inputtext v-model="email" id="email-field" type="email"/>
+          </div>
+          <div>
+            <label for="password-field">Password</label>
+            <app-inputtext v-model="password" id="password-field" type="password"/>
+          </div>
+          <div>
+            <app-button :label="submitButtonText" type="submit" icon="pi pi-user" :loading="isLoading"></app-button>
+          </div>
+        </form>
+    </div>
+  </div>
 </template>
