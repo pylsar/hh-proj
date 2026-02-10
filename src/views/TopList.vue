2e6ea6d2bb1ef5e95d7826d635c8753635f2406f
Alex Kusov <kysov2006@bk.ru>
2026-02-10 21:28:26 +0300

    add prettier

--- src/views/TopList.vue
+++ src/views/TopList.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="toplist__box">
     <h1>Топы</h1>
-    
+
     <app-tabs :value="activeTab">
       <app-tabList>
-        <app-tab 
-          v-for="tab in tabs" 
-          :key="tab.title" 
+        <app-tab
+          v-for="tab in tabs"
+          :key="tab.title"
           :value="tab.value"
           @click="activeTab = tab.value"
         >
@@ -14,27 +14,23 @@
         </app-tab>
       </app-tabList>
       <app-tabPanels>
-        <app-tabPanel 
-          v-for="tab in tabs" 
-          :key="tab.value" 
-          :value="tab.value"
-        >
+        <app-tabPanel v-for="tab in tabs" :key="tab.value" :value="tab.value">
           <div v-if="activeTab === tab.value" class="toplist__items">
-            <div 
-              v-for="(item, index) in tab.data" 
+            <div
+              v-for="(item, index) in tab.data"
               :key="item.id"
               class="toplist__item"
               @click="goToDetail(item.id)"
             >
               <ul class="toplist__item-content">
                 <li>
-                  <span>{{ index + 1}}</span>
+                  <span>{{ index + 1 }}</span>
                   <span>
-                    <i 
+                    <i
                       :class="{
                         'pi pi-check': item.is_read,
-                        'pi pi-times': !item.is_read 
-                      }" 
+                        'pi pi-times': !item.is_read,
+                      }"
                       :style="{
                         color: item.is_read ? 'green' : 'red',
                       }"
@@ -52,44 +48,44 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-import bbcData from '@/assets/db/bbcTop.json'
-import newYorkTimesData from '@/assets/db/newYorkTimes.json'
+import bbcData from '@/assets/db/bbcTop.json';
+import newYorkTimesData from '@/assets/db/newYorkTimes.json';
 // import type {BookBbc} from '@/interfaces'
 
-const router = useRouter()
-const activeTab = ref<string>('0')
+const router = useRouter();
+const activeTab = ref<string>('0');
 
 const tabs = ref([
-    { 
-      title: 'BBC 200', 
-      value: '0',
-      data: bbcData.bbcTop
-    },
-    { 
-      title: 'New York Times 100', 
-      value: '1',
-      data: newYorkTimesData.NewYorkTimes
-    },
-])
+  {
+    title: 'BBC 200',
+    value: '0',
+    data: bbcData.bbcTop,
+  },
+  {
+    title: 'New York Times 100',
+    value: '1',
+    data: newYorkTimesData.NewYorkTimes,
+  },
+]);
 
 const goToDetail = (id: number | string): void => {
-  router.push(`/toplist/${id}`)
-}
+  router.push(`/toplist/${id}`);
+};
 </script>
 
 <style scoped>
-  .toplist__item-content{
-    list-style-type: none;
-  }
+.toplist__item-content {
+  list-style-type: none;
+}
 
-  .toplist__item-content li{
-    display: flex;
-    gap: 8px;
-  }
-  .toplist__item-content li span{
-    min-width: 20px;
-  }
+.toplist__item-content li {
+  display: flex;
+  gap: 8px;
+}
+.toplist__item-content li span {
+  min-width: 20px;
+}
 </style>
