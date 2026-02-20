78c42a97d18761a506b884832ae667115a150b0d
Alex Kusov <kysov2006@bk.ru>
2026-02-20 18:55:38 +0300

    scss

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,91 +1,94 @@
 <script setup lang="ts">
-    import {ref, computed} from 'vue'
-    import type {ComputedRef} from 'vue'
-    import {useUserStore} from '../stores/user';
-    import {getAuth, signOut} from 'firebase/auth'
-    import { useRouter } from 'vue-router';
+import { ref, computed } from 'vue';
+import type { ComputedRef } from 'vue';
+import { useUserStore } from '../stores/user';
+import { getAuth, signOut } from 'firebase/auth';
+import { useRouter } from 'vue-router';
 
-    const userStore = useUserStore();
-    const router = useRouter();
+const userStore = useUserStore();
+const router = useRouter();
 
-    // interface IMenuItem {
-    //     label: string,
-    //     icon: string,
-    //     path: string,
-    //     show: ComputedRef<boolean>
-    // }
+// interface IMenuItem {
+//     label: string,
+//     icon: string,
+//     path: string,
+//     show: ComputedRef<boolean>
+// }
 
-    const items = computed(() => {
-        if (userStore.userId) {
-            return [
-                {
-                    label: 'Добавить',
-                    icon: 'pi pi-plus',
-                    path: '/',
-                    show: true 
-                },
-                {
-                    label: 'Список',
-                    icon: 'pi pi-list',
-                    path: '/list',
-                    show: true
-                },
-                {
-                    label: 'Статистика',
-                    icon: 'pi pi-chart-pie',
-                    path: '/statistic',
-                    show: true
-                },
-                {
-                    label: 'Топы',
-                    icon: 'pi pi-list',
-                    path: '/toplist',
-                    show: true
-                },
-            ]
-        } else {
-            return [
-                {
-                    label: 'Авторизация',
-                    icon: 'pi pi-user',
-                    path: '/auth',
-                    show: true
-                },
-            ]
-        }
-    })
-    const signOutFunc = async(): Promise<void> => {
-        await signOut(getAuth())
-        router.push('/auth')
-    }
+const items = computed(() => {
+  if (userStore.userId) {
+    return [
+      {
+        label: 'Добавить',
+        icon: 'pi pi-plus',
+        path: '/',
+        show: true,
+      },
+      {
+        label: 'Список',
+        icon: 'pi pi-list',
+        path: '/list',
+        show: true,
+      },
+      {
+        label: 'Статистика',
+        icon: 'pi pi-chart-pie',
+        path: '/statistic',
+        show: true,
+      },
+      {
+        label: 'Топы',
+        icon: 'pi pi-list',
+        path: '/toplist',
+        show: true,
+      },
+    ];
+  } else {
+    return [
+      {
+        label: 'Авторизация',
+        icon: 'pi pi-user',
+        path: '/auth',
+        show: true,
+      },
+    ];
+  }
+});
+const signOutFunc = async (): Promise<void> => {
+  await signOut(getAuth());
+  router.push('/auth');
+};
 </script>
 
 <template>
-    <app-menubar :model="items" class="menu">
-        <template #item="{item, props}">
-            <router-link v-if="item.show" :to="item.path" class="menu-item" v-bind="props.action">
-                <span :class="item.icon"/>
-                <span>{{ item.label }}</span>
-            </router-link>
-        </template>
-        <template #end>
-            <span v-if="userStore.userId" @click="signOutFunc" class="sign-out">
-                <span class="pi pi-sign-out"></span>
-                <span>Выход</span>
-            </span>
-        </template>
-    </app-menubar>
-
+  <app-menubar :model="items" class="menu">
+    <template #item="{ item, props }">
+      <router-link
+        v-if="item.show"
+        :to="item.path"
+        class="menu-item"
+        v-bind="props.action"
+      >
+        <span :class="item.icon" />
+        <span>{{ item.label }}</span>
+      </router-link>
+    </template>
+    <template #end>
+      <span v-if="userStore.userId" @click="signOutFunc" class="sign-out">
+        <span class="pi pi-sign-out"></span>
+        <span>Выход</span>
+      </span>
+    </template>
+  </app-menubar>
 </template>
 
-<style>
-    .menu{
-        margin-bottom: 20px;
-    }
-    
-    .menu .sign-out{
-        display: flex;
-        align-items: center;
-        gap: 6px;
-    }
+<style lang="scss">
+.menu {
+  margin-bottom: 20px;
+  .sign-out {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+  }
+}
 </style>
