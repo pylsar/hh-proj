7314bb557b712f224759bd3e21b3bf5467336921
Alex Kusov <kysov2006@bk.ru>
2026-03-01 14:33:27 +0300

    interface comments

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,25 +1,25 @@
 import type { Section, Priority } from '@/stores/listing';
 export interface ISkills {
-  id: string;
-  skillName: string;
-  skillSection: Section;
-  skillDescription: string;
-  skillPrioritys: Priority;
+  id: string; /** id */
+  skillName: string; /** Название элемента */
+  skillSection: Section; /** Раздел элемента */
+  skillDescription: string; /** Описание элемента */
+  skillPrioritys: Priority; /** Приоритет элемента */
   createdAt?: Date;
 }
 
 // книги bbcTop
 export interface BookBbc {
-  id: number | string;
-  name: string;
-  desc: string;
-  author: string;
-  country: string;
-  src: string;
-  year: string | number;
-  likes: string[];
-  is_done: boolean;
-  series: boolean;
-  people: string[];
-  in_rus: boolean;
+  id: number | string; /** id */
+  name: string; /** имя элемента */
+  desc: string; /** описание элемента */
+  author: string; /** автор/режиссер/создатель */
+  country: string; /** Страна в которой создали */
+  src: string; /** путь до картинки */
+  year: string | number; /** Год создания */
+  likes: string[]; /** страны в которых популярна */
+  is_done: boolean; /** завершен/прочитан/просмотрен */
+  series: boolean; /** является ли циклом */
+  people: string[]; /** известные люди которым нравится */
+  in_rus: boolean; /** локализован ли на русский язык */
 }
